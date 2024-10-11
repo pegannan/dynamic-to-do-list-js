3ef9301265a7ae6a09a715581e97fd076a70513b
Peggy Annan <pegannan.22@gmail.com>
2024-10-11 23:11:40 +0000

    add files

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addeventlistener("DOMContentLoaded", (event) => {
+    
+    const addButton = document.getElementById('add-task-btn')
+
+    const taskInput = document.getElementById('task-input')
+
+    const taskList = document.getElementById('task-list')
+
+
+function addTask(){
+    var taskTest = taskInput = ''.trim();
+    if  (taskTest === "") { alert('user:enter a task');}
+    else if (taskTest !== ""){
+        taskList = <li>taskText</li>; <button class="remove-btn" onclick=" ">Remove</button>
+    taskInput.value = ''
+    }
+
+}
+
+addButton(document.addEventListener(onclick,addTask()))
+
+taskInput(document.addEventListener('keypress', addTask(event);
+if event.key = 'Enter'
+));
+
+});
